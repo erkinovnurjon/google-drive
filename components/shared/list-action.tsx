30bc3a8638d789b49bd6af38bf6dae579b26fdab
Nurjon <nurjon2477@gmail.com>
2024-01-02 12:44:56 +0500

    ondelete function

--- components/shared/list-action.tsx
+++ components/shared/list-action.tsx
@@ -2,6 +2,11 @@ import { IFolderAndFile } from "@/types"
 import { Download, MoreVertical, Pencil, Star, Trash, UserPlus } from "lucide-react"
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover"
 import { Separator } from "../ui/separator"
+import { doc, setDoc } from "firebase/firestore"
+import { db } from "@/lib/firebase"
+import { useRouter } from "next/navigation"
+import { toast } from "sonner"
+import React from "react"
 
 
 interface ListActionProps{
@@ -9,10 +14,36 @@ interface ListActionProps{
 }
 
 const ListAction = ({item} : ListActionProps) => {
+      const {refresh} = useRouter()
+
+      const onDelete = (e : React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+         e.stopPropagation()
+
+         const type = item.size ? "files" : "folders"
+
+         const ref =doc(db , type ,  item.id )
+         const promise = setDoc(ref , {...item ,
+             isArchive : true , 
+            archivedTime : new Date()})
+            .then(() => refresh());
+
+            toast.promise(promise , {
+                  loading : "Loading...",
+                  success : " Archived",
+                  error : "Failed to Archive"
+            })
+      }
+
+      const onStar = (e : React.MouseEvent<HTMLDivElement , MouseEvent>) => {
+         e.stopPropagation()
+
+         
+      }
   return (
     <div className="flex items-center space-x-1">
       <div role="button" className="p-2 hover:bg-gray-200 dark:hover:bg-gray-600 rounded-full transition opacity-0
-       group-hover:opacity-100">
+       group-hover:opacity-100"
+       onClick={onDelete}>
             <Trash className=" w-4 h-4 opacity-50" />
       </div>
               <div role="button" className="p-2 hover:bg-gray-200 dark:hover:bg-gray-600 rounded-full transition opacity-0
@@ -40,7 +71,9 @@ const ListAction = ({item} : ListActionProps) => {
                                 <UserPlus className="w-4 h-4" />
                                 <span>Share</span>
                           </div>
-                          <div className=" flex items-center hover:bg-secondary transition py-2 px-4 space-x-2 text-sm" role="button">
+                          <div 
+                           onClick={onDelete}
+                          className=" flex items-center hover:bg-secondary transition py-2 px-4 space-x-2 text-sm" role="button">
                                 <Trash className="w-4 h-4" />
                                 <span>Move to trash</span>
                           </div>
