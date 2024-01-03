f53dd6e3850bb85f7062118a7404c517cdde19bb
Nurjon <nurjon2477@gmail.com>
2024-01-03 16:29:51 +0500

    empty page

--- components/shared/empty.tsx
+++ components/shared/empty.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import React from "react";
 
-const Empty = () => {
-  return (
-    <div>Empty</div>
-  )
+interface EmptyProps {
+      sm?: boolean;
 }
 
-export default Empty
+const Empty = ({ sm }: EmptyProps) => {
+      return (
+            <div className={cn("w-full justify-center flex flex-col space-y-2")}>
+                  <Image
+                        src={"/empty.svg"}
+                        alt="Empty"
+                        width={sm ? 100 : 200}
+                        height={sm ? 100 : 200}
+                        className="mx-auto"
+                  />
+
+                  <h1 className="text-3xl text-center">Empty</h1>
+
+                  <p className="text-center text-gray-500">
+                        There is nothing here. Please check again.
+                  </p>
+            </div>
+      );
+};
+
+export default Empty;
